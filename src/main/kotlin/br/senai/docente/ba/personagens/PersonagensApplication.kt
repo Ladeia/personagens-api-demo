bc9bb901eae3bc1028cb6aa8f37f1d9481649165
ladeia <contato@antonioladeia.com>
2024-06-18 06:10:55 -0300

    Estrutura inicial gerada no spring initializr

---
+++ src/main/kotlin/br/senai/docente/ba/personagens/PersonagensApplication.kt
@@ -0,0 +1,11 @@
+package br.senai.docente.ba.personagens
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonagensApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonagensApplication>(*args)
+}
---
+++ src/test/kotlin/br/senai/docente/ba/personagens/PersonagensApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.senai.docente.ba.personagens
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonagensApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
